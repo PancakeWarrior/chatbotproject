9c5b4606f43c9c5bf0f4eddd1b99ab77cc6de68f
Christopher <cdwyatt01@gmail.com>
2016-09-30 10:34:53 -0700

    got rid of unessasry code ^-^

--- chatbot.js
+++ chatbot.js
@@ -1,25 +1,13 @@
 var response = {
-  "Hi": "Hello! ( ･ω･)ﾉ",
   "hi": "Hello! ( ･ω･)ﾉ",
-  "Hello": "Hi! ( ﾟ▽ﾟ)/",
   "hello": "Hi! ( ﾟ▽ﾟ)/",
-  "How are you?": "I'm doing fine. (*•̀ᴗ•́*)و ̑̑",
   "how are you?": "I'm doing fine. (*•̀ᴗ•́*)و ̑̑",
-  "What time is it?": "If you look in the bottom right hand corner of your laptop you will see. (－‸ლ)",
   "what time is it?": "If you look in the bottom right hand corner of your laptop you will see. (－‸ლ)",
-  "What is your favorite color?": "Red. ♥(ˆ⌣ˆԅ)",
   "what is your favorite color?": "Red. ♥(ˆ⌣ˆԅ)",
-  "What is the meaning of life?": "42. ƪ(•̃͡•̃͡ ƪ",
   "what is the meaning of life?": "42. ƪ(•̃͡•̃͡ ƪ",
-  "What is your favorite animal?": "computer bugs. (¬‿¬)",
   "what is your favorite animal?": "computer bugs. (¬‿¬)",
-  "Who are you?": "I am me. Who are you? ◑.◑",
   "who are you?": "I am me. Who are you? ◑.◑",
-  "How old are you?": "I was created in the month of September 2016. (*^▽^*)",
   "how old are you?": "I was created in the month of September 2016. (*^▽^*)",
-  "What is your name?": "Bubble. (・◇・)",
-  "what is your name?": "Bubble. (・◇・)",
-  "Do you like drawing?": "Sure. ( ﾉ^ω^)ﾉﾟ",
   "do you like drawing?": "Sure. ( ﾉ^ω^)ﾉﾟ",
   "hOi": "hOi!!! °˖✧◝(ਠᴥਠ)◜✧˖°",
   "Boi": "ఠ ͟ಠ",
@@ -31,6 +19,8 @@ var response = {
 
 function speak() {
   var userInput = $("#input").val();
+  userInput = userInput.toLowerCase();
+
   var answer = response[userInput];
   if(answer == undefined) {
     var fallBackAnswer = ["ɿ(｡･ɜ･)ɾⓌⓗⓨ？","ɿ(｡･ɜ･)ɾⓌⓗⓐⓣ？","(ㆀ˘･з･˘)ωҺaｔ？","Ⓦⓗⓐⓣ(☉൧ ಠ ꐦ)","ʅฺ(・ω・。)ʃฺ？？"];
